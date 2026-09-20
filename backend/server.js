0c6da15cbd287cd6ccb43010695da8fbeeb3b9e4
Sahil Chaudhary <Sahiljakhar6030@gmail.com>
2026-09-20 11:14:34 +0530

    Update CORS for live frontend

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,8 @@ app.use(
     cors({
         origin: [
             "http://localhost:3000",
-            "http://localhost:3001"
+            "http://localhost:3001",
+            "https://food-xxxxx.vercel.app"
         ],
         credentials: true
     })
